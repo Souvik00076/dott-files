25b08a1630e3dfe480ff16f56bb912a967f7e728
Souvik00076 <souvikbhattacharjee00076@gmail.com>
2025-06-07 19:36:56 +0000

    New LSP added(JDLTS,Python) , redundant code removed

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,36 @@
+-- return {
+--   {
+--     "craftzdog/solarized-osaka.nvim",
+--     lazy = true,
+--     priority = 1000,
+--     opts = function()
+--       return {
+--         transparent = true,
+--       }
+--     end,
+--   },
+-- }
+--
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
+
+return { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... }
+-- return {
+--   { "nyoom-engineering/oxocarbon.nvim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "oxocarbon",
+--     },
+--   },
+-- }
